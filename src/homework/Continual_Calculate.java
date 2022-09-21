c3c5a4ce748338df255c3166e029c6cc848050d4
yoodongbin <ehdqls0412@naver.com>
2022-09-21 12:34:53 +0900

    결과는 나옴, 계산기 여려가지 시도 (stream, fori ...)

--- src/homework/Continual_Calculate.java
+++ src/homework/Continual_Calculate.java
@@ -1,67 +1,72 @@
 package homework;
 
 import java.util.ArrayList;
+import java.util.OptionalInt;
 import java.util.Scanner;
-import java.util.stream.IntStream;
 
 public class Continual_Calculate {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = 0;
+        String c = "";
+        ArrayList<Integer> arrayList = new ArrayList<>();
         while (true) {
-            Scanner scanner = new Scanner(System.in);
-            int a = 0;
-            String c = "";
-            ArrayList arrayList = new ArrayList<>();
+
             System.out.println("param : ");
             if (scanner.hasNextInt()) {
                 a = scanner.nextInt();
                 arrayList.add(a);
             }else {
                 c = scanner.next();
-
                 calculate(arrayList, c);
+                arrayList.clear();
             }
         }
     }
 
-    public static void calculate(ArrayList arrayList, String c) {
-            int result = 0;
+    public static void calculate(ArrayList<Integer> arrayList, String c) {
             switch (c) {
                 case "+":
-                    result = add(arrayList);
+                    System.out.println(arrayList + " add 결과 = " + add(arrayList));
                     break;
-//                case "-":
-//                    result = min(a, b);
-//                    break;
-//                case "*":
-//                    result = mul(a, b);
-//                    break;
-//                case "/":
-//                    if (b == 0) {
-//                        result = 0;
-//                        break;
-//                    }
-//                    result = div(a, b);
-//                    break;
+                case "-":
+                    System.out.print(arrayList);
+                    System.out.println( " minus 결과 = " + min(arrayList)+"\n");
+                    break;
+                case "*":
+                    System.out.println(arrayList + " mul 결과 = " + mul(arrayList));
+                    break;
+                case "/":
+                    System.out.println(arrayList + " div 결과 = " + div(arrayList));
+                    break;
             }
-            System.out.println("결과 : " + result);
     }
 
-    public static int add(ArrayList arrayList) {
-        for (integer item : arrayList){
-            return item;
+    public static int add(ArrayList<Integer> arrayList) {
+       int add_result = arrayList.stream().mapToInt(Integer::intValue).sum();
+       return add_result;
+    }
+
+    public static int min(ArrayList<Integer> arrayList) {
+        int min_result = arrayList.get(0);
+        arrayList.remove(0);
+        for (int i : arrayList) {
+            if (i == 0) {
+                min_result = arrayList.get(i);
+            }
+            min_result -= i;
         }
+        return min_result;
     }
-
-    public static int min(int a, int b) {
-        return a-b;
+//
+    public static int mul(ArrayList<Integer> arrayList) {
+        OptionalInt mul_result = arrayList.stream().mapToInt(Integer::intValue).reduce((a, b)-> a * b );
+        return mul_result.getAsInt();
     }
-
-    public static int mul(int a, int b) {
-        return a*b;
-    }
-
-    public static int div(int a, int b) {
-        return a/b;
-    }
+//
+    public static int div(ArrayList<Integer> arrayList) {
+        OptionalInt div_result = arrayList.stream().mapToInt(Integer::intValue).reduce((a, b) -> a / b);
+        return div_result.getAsInt();
+        }
 }
 
